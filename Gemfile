73f4901ef92e71ea024326aeb2af5fea085b7a0a
Noel Curtis <noelcurtis@gmail.com>
2011-05-22 22:42:18 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.7'
+
+gem 'sqlite3'
+
+group :development do
+  gem 'rspec-rails', '2.6.0'
+  gem 'faker', '0.9.5'
+end
+
+group :test do
+  gem 'rspec-rails', '2.6.0'
+  gem 'webrat'
+  gem 'factory_girl_rails', '1.0.1'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Minercloud::Application.config.secret_token = '6746b01a767a50e65c22a666b18358f8ed932b536ad2db0d678fb12d3863223665d26740c98de272f33a0e0aca5480fa7d1cef296d1626ebc9e8805e404f6dcd'
